6f33cc80d5146ddccf237f23345d434ffba17aba
arjunatlast <arjunatlast@gmail.com>
2018-07-26 20:07:15 +0530

    Patch CHanges

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -30,30 +30,30 @@ export class HomePageComponent implements OnInit {
 
   async fetchProducts(limit: number) {
 
-    //this.products = await this.ps.getProducts(limit);
+    this.products = await this.ps.getProducts(limit);
 
     //console.log(this.products.toString((product) => JSON.stringify(product)));
 
-    this.products = new ArrayList<Product>(limit, ...[
-      {
-        "salesCount":0,
-        "description":"Delicious Crispy and Tasty French Fries",
-        "discount":10,
-        "featured":true,
-        "unit":"piece",
-        "name":"McCain French Fries",
-        "category":{
-          "imageUrl":"https://firebasestorage.googleapis.com/v0/b/ajmah-2a334.appspot.com/o/images%2Fcategories%2F3rLLQ7jeRZpqwtBg5nto?alt=media&token=29869d75-1d3b-49d4-9523-7fa4b34ec5b2",
-          "id":"3rLLQ7jeRZpqwtBg5nto",
-          "productCount":1,
-          "name":"Bakery",
-          "salesCount":0
-        },
-        "id":"wqEbunhdsGHmG5k9wNW4",
-        "price":30,
-        "featuredImageUrl":"https://firebasestorage.googleapis.com/v0/b/ajmah-2a334.appspot.com/o/images%2Fproducts%2FwqEbunhdsGHmG5k9wNW4?alt=media&token=9bffc043-9c6b-40b9-a241-ee0d0b256a1b"
-      }
-    ]);
+    // this.products = new ArrayList<Product>(limit, ...[
+    //   {
+    //     "salesCount":0,
+    //     "description":"Delicious Crispy and Tasty French Fries",
+    //     "discount":10,
+    //     "featured":true,
+    //     "unit":"piece",
+    //     "name":"McCain French Fries",
+    //     "category":{
+    //       "imageUrl":"https://firebasestorage.googleapis.com/v0/b/ajmah-2a334.appspot.com/o/images%2Fcategories%2F3rLLQ7jeRZpqwtBg5nto?alt=media&token=29869d75-1d3b-49d4-9523-7fa4b34ec5b2",
+    //       "id":"3rLLQ7jeRZpqwtBg5nto",
+    //       "productCount":1,
+    //       "name":"Bakery",
+    //       "salesCount":0
+    //     },
+    //     "id":"wqEbunhdsGHmG5k9wNW4",
+    //     "price":30,
+    //     "featuredImageUrl":"https://firebasestorage.googleapis.com/v0/b/ajmah-2a334.appspot.com/o/images%2Fproducts%2FwqEbunhdsGHmG5k9wNW4?alt=media&token=9bffc043-9c6b-40b9-a241-ee0d0b256a1b"
+    //   }
+    // ]);
     
   }
 
